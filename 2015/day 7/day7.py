a9e1d0e6d69653b9706a1f52b719a136f1891632
Jack Morgan <jack@morgans.org.uk>
2021-12-01 07:39:10 +0100

    remove old files

--- 2015/day 7/day7.py
+++
@@ -1,7 +0,0 @@
-#circuit description as dictionary
-
-with open('data.in', 'r') as f:
-    C = {l.strip()[l.index('->')+3:]: l[:l.index('->')-1] for l in f}
-    #cmd, num wires, out
-    print(C)
-
