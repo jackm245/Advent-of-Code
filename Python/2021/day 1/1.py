e007aa58bd79ff9a88486a83e8d006faae9ce218
Jack Morgan <jack@morgans.org.uk>
2021-12-01 16:49:15 +0100

    update 2021 day 1

--- Python/2021/day 1/1.py
+++ Python/2021/day 1/1.py
@@ -2,20 +2,28 @@ import sys
 
 def get_data():
     with open(sys.argv[1], 'r') as f:
-        # L = [int(l.strip()) for l in f]
-        L = [l.strip() for l in f]
+         L = [int(l.strip()) for l in f]
     return L
 
 
 def part_one(L):
-    pass
+    count = 0
+    for i in range(1, len( L)):
+        if L[i - 1] < L[i]:
+            count += 1
+    return count
 
 
 def part_two(L):
-    pass
-
+    count = 0
+    for i in range(len(L) - 3):
+        if sum(L[i:i+3]) < sum(L[i+1:i+4]):
+            count += 1
+    return count
 
 L = get_data()
-#  print(L)
 print(f'part one: {part_one(L)}')
 print(f'part two: {part_two(L)}')
+
+
+
